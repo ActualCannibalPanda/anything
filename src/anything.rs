a3d10b2219d7bac2743ec78a684cba690694f1cf
Robert O'Shea <robertosheagamedev@gmail.com>
2025-01-07 19:38:06 +0000

    Refactor

--- src/anything.rs
+++ src/anything.rs
@@ -10,8 +10,8 @@ pub trait ToBox<T: ?Sized + Cast>: Any {
 }
 
 pub trait Cast {
-    unsafe fn downcast_ref<T>(&self) -> &T;
-    unsafe fn downcast_mut<T>(&mut self) -> &mut T;
+    unsafe fn cast_ref<T>(&self) -> &T;
+    unsafe fn cast_mut<T>(&mut self) -> &mut T;
 }
 
 pub struct Map<A: ?Sized = dyn Any> {
@@ -30,13 +30,13 @@ impl<A: ?Sized + Cast> Map<A> {
     pub fn get<T: ToBox<A>>(&self) -> Option<&T> {
         self.raw
             .get(&TypeId::of::<T>())
-            .map(|any| unsafe { any.downcast_ref::<T>() })
+            .map(|any| unsafe { any.cast_ref::<T>() })
     }
 
     pub fn get_mut<T: ToBox<A>>(&mut self) -> Option<&mut T> {
         self.raw
             .get_mut(&TypeId::of::<T>())
-            .map(|any| unsafe { any.downcast_mut() })
+            .map(|any| unsafe { any.cast_mut() })
     }
 
     pub fn insert<T: ToBox<A>>(&mut self, value: T) {
@@ -47,11 +47,11 @@ impl<A: ?Sized + Cast> Map<A> {
 macro_rules! generate_implementation {
     ($t:ident $(+ $othert:ident)*) => {
         impl Cast for dyn $t $(+ $othert)* {
-            unsafe fn downcast_ref<T>(&self) -> &T {
+            unsafe fn cast_ref<T>(&self) -> &T {
                 &*(self as *const Self as *const T)
             }
 
-            unsafe fn downcast_mut<T>(&mut self) -> &mut T {
+            unsafe fn cast_mut<T>(&mut self) -> &mut T {
                 &mut *(self as *mut Self as *mut T)
             }
         }
